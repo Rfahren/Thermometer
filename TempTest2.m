d4051eaeddaeed4686269da493da8f4507bae51f
Rfahren <Lotblue5@aol.com>
2018-01-06 19:40:27 -0700

    modified temptest 2 to example 7 on mathworks

--- TempTest2.m
+++ TempTest2.m
@@ -1,17 +1,21 @@
 clc
 clear all
 a=arduino();
-c1=2.108508173e-3;
-c2=0.7979204727e-4;
-c3=0.7979204727e-7;
-R=10000;
-for t=1:10
-    t=t+1;
-    v=readVoltage(a,'A0');
-    R2 =log( R * ((1024./v)- 1));
-    T = (1 / (c1 + (c2 + c3*R2*R2)*R2)); % Steinhart–Hart equation
-    tempC = T - 273.15;            
-    tempF = (tempC * 9.0)/ 5.0 + 32.0;
-    fprintf('v=%2.4f\n',v);
+%c1=2.108508173e-3;
+%c2=0.7979204727e-4;
+%c3=0.7979204727e-7;
+%R=10000;
+tempPin='A0';
+tempCfromVolts=@(volts) (volts-0.5)*10;
+%samplingTimes=0:2:30;
+%numSamples=length(samplingTimes);
+tempC=zeros(60,1);
+tempF=tempC;
+for index=1:60
+	volts=readVoltage(a,tempPin);
+	tempC(index)=tempCfromVolts(volts);
+	tempF(index)=tempC(index)*9/5+32;
+	
+	fprintf('Temp  %5.2f F \n',tempF(index));
 end
-clear a;
+clear a
